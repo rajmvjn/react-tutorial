76441fc81f0efb82a3f199c5e44543281bc4a244
rajmvjn <rajmvjn@gmail.com>
2019-10-01 08:47:19 +0530

    Updates

---
+++ precache-manifest.aee115eee1a22d9085f0dfd23f9901ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9097dfd8367f5685eda30d408c24b0f7",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "0889f35519aa8ab6f915",
+    "url": "/react-tutorial/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "f4f92a02e7d1f3f75c34",
+    "url": "/react-tutorial/static/js/2.2eeaa575.chunk.js"
+  },
+  {
+    "revision": "0889f35519aa8ab6f915",
+    "url": "/react-tutorial/static/js/main.76f400bc.chunk.js"
+  },
+  {
+    "revision": "097e5acec69a01711434",
+    "url": "/react-tutorial/static/js/runtime-main.7d39e862.js"
+  }
+]);
